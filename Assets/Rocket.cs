876b0bef5e532aae7f8a7c88f6813d7afbc61083
Kyle Chadwick <kylepchad@gmail.com>
2017-11-21 22:21:54 -0800

    rocket thrust, rotation, sound

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,27 +6,42 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        ProcessInput();	
+    void Update () {
+        Thrust();
+        Rotate();	
 	}
 
-    private void ProcessInput() {
-        if (Input.GetKey(KeyCode.Space)){ // can thrust while rotating
+    private void Thrust() {
+        if (Input.GetKey(KeyCode.Space)) { // can thrust while rotating
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) { // so it doesnt layer
+                audioSource.Play();
+            }
         }
+        else {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate() {
+        rigidBody.freezeRotation = true; // take manual control of rotation
 
         if (Input.GetKey(KeyCode.A)) {
-            print("Rotating left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D)) {
-            print("Rotating right");
+            transform.Rotate(Vector3.back);
         }
+
+        rigidBody.freezeRotation = false; // resume physics control of rotaiton
     }
 }
